00052509cebed540656ca5b995ee7e8d42d06dad
Justin Zeng <hi.justinzeng@gmail.com>
2021-01-22 22:20:21 -0800

    completed highest scoring word

---
+++ arrays/highest_scoring_word/highest_scoring_word.go
@@ -0,0 +1,33 @@
+package highest_scoring_word
+
+import "strings"
+
+func High(s string) string {
+	alphabets := "abcdefghijklmnopqrstuvwxyz"
+	words := strings.Split(s, " ")
+
+	var wordsSum []int
+
+	for _, word := range words {
+		letters := strings.Split(word, "")
+		sum := 0
+
+		for _, letter := range letters {
+			sum += strings.Index(alphabets, letter) + 1
+		}
+
+		wordsSum = append(wordsSum, sum)
+	}
+
+	var maxSum int
+	var maxSumIdx int
+
+	for idx, wordSum := range wordsSum {
+		if wordSum > maxSum {
+			maxSum = wordSum
+			maxSumIdx = idx
+		}
+	}
+
+	return words[maxSumIdx]
+}
